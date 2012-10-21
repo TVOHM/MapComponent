3816e6429d556058d4a0e6334e98cae3ab0553d9
Ian Edwards <ianpedwards87@gmail.com>
2012-10-21 14:00:12 +0100

    Initial working map implementation commit

--- src/com/mim/mapcomponent/MainActivity.java
+++ src/com/mim/mapcomponent/MainActivity.java
@@ -1,15 +1,20 @@
 package com.mim.mapcomponent;
 
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends MapActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
     }
 
     @Override
@@ -17,4 +22,10 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
