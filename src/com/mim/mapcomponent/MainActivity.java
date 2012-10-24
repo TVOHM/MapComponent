afe5d41f675b994003618c0aafc74f1a82d85d58
Ian Edwards <ianpedwards87@gmail.com>
2012-10-24 10:10:51 +0100

    Initial GPS commit

--- src/com/mim/mapcomponent/MainActivity.java
+++ src/com/mim/mapcomponent/MainActivity.java
@@ -3,18 +3,38 @@ package com.mim.mapcomponent;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.app.Activity;
+//import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends MapActivity {
+public class MainActivity extends MapActivity implements LocationListener {
+	
+	private LocationManager mLocationManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 5.0f, this);
         MapView mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
+    }
+    
+    @Override
+    public void onPause() {
+    	super.onPause();
+    	mLocationManager.removeUpdates(this);
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 5.0f, this);
     }
 
     @Override
@@ -27,5 +47,25 @@ public class MainActivity extends MapActivity {
 	protected boolean isRouteDisplayed() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onProviderEnabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+		
 	}
 }
